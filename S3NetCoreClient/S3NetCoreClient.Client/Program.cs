0ee85c717620b66b9088f8258535394cd751fb21
Ravi Vaddadi <ravindranath.vaddadi@wolterskluwer.com>
2018-04-12 19:41:27 -0400

    Added Service layer and console app

---
+++ S3NetCoreClient/S3NetCoreClient.Client/Program.cs
@@ -0,0 +1,50 @@
+﻿using Amazon.S3.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace S3NetCoreClient.Client
+{
+    internal class Program
+    {
+        private static HttpClient _client;
+
+        public static HttpClient Client
+        {
+            get
+            {
+                _client = _client ?? new HttpClient();
+                return _client;
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            Client.BaseAddress = new Uri("http://localhost:22831/");
+            Client.DefaultRequestHeaders.Accept.Clear();
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            List<S3Bucket> buckets = GetBuckets().GetAwaiter().GetResult();
+            foreach (S3Bucket bucket in buckets)
+            {
+                Console.WriteLine(bucket.BucketName);
+            }
+            Console.WriteLine("Hello World!");
+        }
+
+        private async static Task<List<S3Bucket>> GetBuckets()
+        {
+            List<S3Bucket> buckets = new List<S3Bucket>();
+            HttpResponseMessage response = await Client.GetAsync("api/S3BucketController");
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonResult = await response.Content.ReadAsStringAsync();
+                buckets = JsonConvert.DeserializeObject<List<S3Bucket>>(jsonResult);
+            }
+            return buckets;
+        }
+    }
+}
---
+++ S3NetCoreClient/S3NetCoreClient.Service/Controllers/S3BucketController.cs
@@ -0,0 +1,51 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace S3NetCoreClient.Service.Controllers
+{
+    [Route("api/[controller]")]
+    public class S3BucketController : Controller
+    {
+        private IAmazonS3 _s3Client;
+
+        public S3BucketController(IAmazonS3 client)
+        {
+            this._s3Client = client;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<S3Bucket> GetAsync()
+        {
+            ListBucketsResponse listBucketsResponse = await _s3Client.ListBucketsAsync();
+            return listBucketsResponse.Buckets;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
